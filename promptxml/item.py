2caf9366e4e1ea69375fb72f35ef4acaa208e603
sushka <r.m.poltorabatko@gmail.com>
2024-07-13 16:23:34 +0300

    Added escaping (#4)
    * ✨ feat: добавил экранирование атрибутов в `promptxml/item.py`
    
    * 🔧 fix: заменил `quoteattr` на `escape` в `promptxml/item.py`

--- promptxml/item.py
+++ promptxml/item.py
@@ -1,4 +1,5 @@
 from typing import Iterable, Self
+from xml.sax.saxutils import escape
 
 
 class PromptItem:
@@ -7,7 +8,7 @@ class PromptItem:
         self.value = value
 
     def to_xml(self) -> str:
-        return f"<{self.label}>{self.value}</{self.label}>"
+        return f"<{self.label}>{escape(self.value, entities={})}</{self.label}>"
 
     @classmethod
     def build_multiple(cls, *, label: str, values: Iterable[str]) -> list[Self]:
